a13cc66da5e12f35b4c401c2b7dd17ccacbe55cf
priyanka <p.priyapatil579@gmail.com>
2025-12-06 00:27:03 +0530

    remove m2 method

--- src/test/java/com/qa/tests/facebooklogintest.java
+++ src/test/java/com/qa/tests/facebooklogintest.java
@@ -3,11 +3,6 @@ package com.qa.tests;
 import org.testng.annotations.Test;
 
 public class facebooklogintest {
-
-	@Test
-	public void m2() {
-		System.out.println("m2");
-			}
 	@Test
 	public void m3() {
 		System.out.println("m3");
