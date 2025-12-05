d7d6cde85ee04b7db463a14ee192748d49a51886
priyanka <p.priyapatil579@gmail.com>
2025-12-05 23:53:40 +0530

    changed isfacebooktitlrtest

--- src/test/java/com/qa/tests/facebooklogintest.java
+++ src/test/java/com/qa/tests/facebooklogintest.java
@@ -1,5 +1,15 @@
 package com.qa.tests;
 
+import org.testng.annotations.Test;
+
 public class facebooklogintest {
 
+	public void m3() {
+		System.out.println("m3");
+			}
+	
+	@Test
+	public void M4() {
+		System.out.println("m4");
+	}
 }
