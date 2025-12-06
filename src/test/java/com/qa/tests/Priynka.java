d95d7b6a9877388c6c0f80436d761090dcbed91c
priyanka <p.priyapatil579@gmail.com>
2025-12-06 15:52:36 +0530

    aaded priyanka

---
+++ src/test/java/com/qa/tests/Priynka.java
@@ -0,0 +1,14 @@
+package com.qa.tests;
+
+import org.testng.annotations.Test;
+
+public class Priynka {
+
+	@Test
+	public void priyanature() {
+		System.out.println("priyanka is good girl");
+		System.out.println("priyanka will have good job");
+		System.out.println("swami and sai karupa always on her");
+		
+	}
+}
