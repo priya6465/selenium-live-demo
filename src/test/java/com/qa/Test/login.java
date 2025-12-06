2e9d83bc1b0b5e173093de6b7cd7cdc44ecc7d83
priyanka <p.priyapatil579@gmail.com>
2025-12-06 20:44:20 +0530

    5 method

--- src/test/java/com/qa/Test/login.java
+++ src/test/java/com/qa/Test/login.java
@@ -20,4 +20,8 @@ public class login {
 	public void m4() {
 		System.out.println("this is m4 method of login class");
 	}
+	@Test(priority=1)
+	public void m5() {
+		System.out.println("this is m5 method of login class");
+	}
 }
